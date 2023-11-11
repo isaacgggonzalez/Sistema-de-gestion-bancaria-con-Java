7e7e2db080f010a64216dd340fd56cb2e557b4f5
isaacgggonzalez <gonzalez.isaac.ulises.2001@fpuna.edu.py>
2023-11-11 16:41:21 -0300

    cambiosentarjeta

--- primer_final/src/primer_final/TarjetaDeCredito.java
+++ primer_final/src/primer_final/TarjetaDeCredito.java
@@ -4,10 +4,10 @@ public class TarjetaDeCredito {
     private double limite_tarjeta;
     private String vencimiento_tarjeta;
     private String nro_tarjeta;
-    private int cliente_asociado;  
+    private Cliente cliente_asociado;  
 
     // Constructor
-    public TarjetaDeCredito(double deuda_tarjeta, double limite_tarjeta, String vencimiento_tarjeta, String nro_tarjeta, int cliente_asociado) {
+    public TarjetaDeCredito(double deuda_tarjeta, double limite_tarjeta, String vencimiento_tarjeta, String nro_tarjeta, Cliente cliente_asociado) {
         this.deuda_tarjeta = deuda_tarjeta;
         this.limite_tarjeta = limite_tarjeta;
         this.vencimiento_tarjeta = vencimiento_tarjeta;
@@ -49,11 +49,11 @@ public class TarjetaDeCredito {
         this.nro_tarjeta = nro_tarjeta;
     }
 
-    public int getCliente_asociado() {
+    public Cliente getCliente_asociado() {
         return cliente_asociado;
     }
 
-    public void setCliente_asociado(int cliente_asociado) {
+    public void setCliente_asociado(Cliente cliente_asociado) {
         this.cliente_asociado = cliente_asociado;
     }
 
